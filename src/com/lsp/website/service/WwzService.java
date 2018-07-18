bd3c527a2e384865f3cad52e43b9f695bef178e2
Administrator <Administrator@PC-20180601KVDZ>
2018-07-18 20:03:08 +0800

    zyl

--- src/com/lsp/website/service/WwzService.java
+++ src/com/lsp/website/service/WwzService.java
@@ -404,7 +404,7 @@ public class WwzService {
 		if(db!=null){
 			UserInfo info=(UserInfo) UniObject.DBObjectToObject(db, UserInfo.class);
 			info.set_id(fromUserid);
-			if(info.getRenumber()==0&&StringUtils.isNotEmpty(agid)&&Long.parseLong(agid)>1000&&!agid.equals("null")&&agid!=null){
+			if(info.getRenumber()==0&&StringUtils.isNotEmpty(agid)&&!agid.equals("null")&&Long.parseLong(agid)>1000&&agid!=null){
 				info.setRenumber(Long.parseLong(agid));
 				baseDao.insert(PubConstants.USER_INFO, info);
 				return true;
